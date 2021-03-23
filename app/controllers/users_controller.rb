e03028eba67f0865e1df1b8edcb7fc8ee222a69b
chinweokwu <paulcmorah@gmail.com>
2021-03-23 21:42:14 +0100

    add status option to user

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -3,7 +3,8 @@ class UsersController < ApplicationController
 
   # GET /users or /users.json
   def index
-    @users = User.all
+    @users = User.order(updated_at: :desc).search(params[:search]).page(params[:page]).per_page(25)
+    @user = User.new
   end
 
   # GET /users/1 or /users/1.json
@@ -25,9 +26,11 @@ class UsersController < ApplicationController
 
     respond_to do |format|
       if @user.save
+        format.js
         format.html { redirect_to @user, notice: "User was successfully created." }
         format.json { render :show, status: :created, location: @user }
       else
+        format.js
         format.html { render :new, status: :unprocessable_entity }
         format.json { render json: @user.errors, status: :unprocessable_entity }
       end
